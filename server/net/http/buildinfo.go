11c9237184c4def54699151a4bc32c07201140f2
dape <d.m.petersson@gmail.com>
2024-03-16 08:23:37 +0100

    add a build info handler

---
+++ server/net/http/buildinfo.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+	"runtime/debug"
+)
+
+func BuildInfoHandler(w http.ResponseWriter, req *http.Request) {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		http.Error(w, "no build info", http.StatusNotImplemented)
+	}
+
+	res := map[string]string{}
+	for _, s := range bi.Settings {
+		if s.Key == "vcs.revision" || s.Key == "vcs.time" || s.Key == "vcs.modified" {
+			res[s.Key] = s.Value
+		}
+	}
+
+	enc := json.NewEncoder(w)
+	enc.Encode(res)
+}
