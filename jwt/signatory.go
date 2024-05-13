5050a3f3a2e9b88ad973920dd8f8150a1e6291ee
dape <d.m.petersson@gmail.com>
2024-05-13 05:46:05 +0200

    create HMACSignatory for simpler usage

--- jwt/signatory.go
+++ jwt/signatory.go
@@ -4,12 +4,12 @@ import (
 	jwt "github.com/golang-jwt/jwt/v5"
 )
 
-type Signatory struct {
-	Key    string
+type HMACSignatory struct {
+	Key    []byte
 	Method jwt.SigningMethod
 }
 
-func (s *Signatory) Sign(c *Claims) (string, error) {
+func (s *HMACSignatory) Sign(c *Claims) (string, error) {
 	raw := jwt.NewWithClaims(s.Method, c)
 	return raw.SignedString(s.Key)
 }
---
+++ jwt/signatory_test.go
@@ -0,0 +1,37 @@
+package jwt
+
+import (
+	"testing"
+
+	jwt "github.com/golang-jwt/jwt/v5"
+)
+
+func TestSign(t *testing.T) {
+	tests := []struct {
+		signatory interface {
+			Sign(*Claims) (string, error)
+		}
+		want string
+	}{
+		{
+			&HMACSignatory{[]byte("the-key"), jwt.SigningMethodHS512},
+			"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWxwaGEgQnJhdm8iLCJpc3MiOiJNN0MuY28ifQ.WcJOo0qGbHZB5yVj-O7K_mZ8lQhJ_FSJNYe2BjpNDx5kMsxv_mHh5bE8F3EjxjKDl8Kv8rzvUC6JfbntpC-QDQ",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Log(tc)
+
+		c := &Claims{
+			"Alpha Bravo",
+			jwt.RegisteredClaims{
+				Issuer: "M7C.co",
+			},
+		}
+
+		g, err := tc.signatory.Sign(c)
+		if err != nil || g != tc.want {
+			t.Fatalf("wrong result; got:%q want:%q err:%+v", g, tc.want, err)
+		}
+	}
+}
