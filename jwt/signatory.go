efc908803f4da4788185a6784ff14aa4efcf8745
dape <d.m.petersson@gmail.com>
2024-07-08 06:33:35 +0200

    fix the signature for signatore

--- jwt/signatory.go
+++ jwt/signatory.go
@@ -9,12 +9,12 @@ type HMACSignatory struct {
 	Method jwt.SigningMethod
 }
 
-func (s *HMACSignatory) Sign(c *Claims) (string, error) {
+func (s *HMACSignatory) Sign(c jwt.Claims) (string, error) {
 	raw := jwt.NewWithClaims(s.Method, c)
 	return raw.SignedString(s.Key)
 }
 
-func (s *HMACSignatory) Parse(c *Claims, txt string) (*jwt.Token, error) {
+func (s *HMACSignatory) Parse(c jwt.Claims, txt string) (*jwt.Token, error) {
 	return jwt.ParseWithClaims(txt, c, func(t *jwt.Token) (interface{}, error) {
 		return s.Key, nil
 	})
--- jwt/signatory_test.go
+++ jwt/signatory_test.go
@@ -6,29 +6,37 @@ import (
 	jwt "github.com/golang-jwt/jwt/v5"
 )
 
+type AppClaims struct {
+	Claims
+	SubscriptionExpiresAt int64 `json:"subexp"`
+}
+
 func TestSignAndParse(t *testing.T) {
 	tests := []struct {
 		signatory interface {
-			Sign(*Claims) (string, error)
-			Parse(*Claims, string) (*jwt.Token, error)
+			Sign(jwt.Claims) (string, error)
+			Parse(jwt.Claims, string) (*jwt.Token, error)
 		}
 		want string
 	}{
 		{
 			&HMACSignatory{[]byte("the-key"), jwt.SigningMethodHS512},
-			"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWxwaGEgQnJhdm8iLCJlbWFpbCI6ImFscGhhQGJyYXZvLmNvbSIsImlzcyI6Ik03Qy5jbyJ9.lK9QUtOzR-I4LUlX83xfc_c5zfsSq1P-XHGybztBvBP2TkAfdyySP5oWaHKRtVQHB_FcN3vX4pwxV9ebfM5fsg",
+			"eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWxwaGEgQnJhdm8iLCJlbWFpbCI6ImFscGhhQGJyYXZvLmNvbSIsImlzcyI6Ik03Qy5jbyIsInN1YmV4cCI6NDg4NjcxODM0NX0.tHlLYzekbbrOWnR3sl5_h3lBrVjMhGmg4VuR4hgHXchrJZ55L6UpWr6-cdpT-4KsqrQKUFf6Ampp_ih0ucg_YQ",
 		},
 	}
 
 	for _, tc := range tests {
 		t.Log(tc)
 
-		c := &Claims{
-			"Alpha Bravo",
-			"alpha@bravo.com",
-			jwt.RegisteredClaims{
-				Issuer: "M7C.co",
+		c := AppClaims{
+			Claims{
+				"Alpha Bravo",
+				"alpha@bravo.com",
+				jwt.RegisteredClaims{
+					Issuer: "M7C.co",
+				},
 			},
+			0x123456789,
 		}
 
 		g, err := tc.signatory.Sign(c)
@@ -36,11 +44,17 @@ func TestSignAndParse(t *testing.T) {
 			t.Fatalf("wrong result; got:%q want:%q err:%+v", g, tc.want, err)
 		}
 
-		c2 := Claims{}
+		c2 := AppClaims{}
 		tok, err := tc.signatory.Parse(&c2, g)
 		if err != nil {
 			t.Fatalf("failed to parse token; err:%+v", err)
 		}
 		t.Log(tok)
+		t.Log(c)
+		t.Log(c2)
+
+		if c2.Name != "Alpha Bravo" || c2.Email != "alpha@bravo.com" || c2.Issuer != "M7C.co" || c2.SubscriptionExpiresAt != 0x123456789 {
+			t.Fatal("Parse don't produce the expected results")
+		}
 	}
 }
